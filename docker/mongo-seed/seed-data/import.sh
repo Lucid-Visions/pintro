553ada2e58773f21877a153995f40196537cc311
mcpolandc <mcpolandc@gmail.com>
2020-06-01 15:50:11 +0100

    More fixes for data

--- docker/mongo-seed/seed-data/import.sh
+++ docker/mongo-seed/seed-data/import.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-mongoimport --host mongo --db pintro --collection actionbuttons --type json --file /mongo-seed/action-buttons.json --jsonArray
-mongoimport --host mongo --db pintro --collection articles --type json --file /mongo-seed/articles.json --jsonArray
-mongoimport --host mongo --db pintro --collection businesses --type json --file /mongo-seed/businesses.json --jsonArray
-mongoimport --host mongo --db pintro --collection notifications --type json --file /mongo-seed/notifications.json --jsonArray
-mongoimport --host mongo --db pintro --collection relationships --type json --file /mongo-seed/relationships.json --jsonArray
-mongoimport --host mongo --db pintro --collection status --type json --file /mongo-seed/status.json --jsonArray
-mongoimport --host mongo --db pintro --collection users --type json --file /mongo-seed/users.json --jsonArray
+mongoimport --host mongo --db pintro --collection actionButtons --type json --file /seed-data/action-buttons.json --jsonArray -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase admin
+mongoimport --host mongo --db pintro --collection articles --type json --file /seed-data/articles.json --jsonArray -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase admin
+mongoimport --host mongo --db pintro --collection businesses --type json --file /seed-data/businesses.json --jsonArray -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase admin
+mongoimport --host mongo --db pintro --collection notifications --type json --file /seed-data/notifications.json --jsonArray -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase admin
+mongoimport --host mongo --db pintro --collection relationship --type json --file /seed-data/relationship.json --jsonArray -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase admin
+mongoimport --host mongo --db pintro --collection hubs --type json --file /seed-data/hubs.json --jsonArray -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase admin
+mongoimport --host mongo --db pintro --collection status --type json --file /seed-data/status.json --jsonArray -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase admin
+mongoimport --host mongo --db pintro --collection users --type json --file /seed-data/users.json --jsonArray -u ${MONGO_INITDB_ROOT_USERNAME} -p ${MONGO_INITDB_ROOT_PASSWORD} --authenticationDatabase admin
