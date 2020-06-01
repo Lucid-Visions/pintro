0af87c7a59d6af519d46afc2480a1888e245bf85
mcpolandc <mcpolandc@gmail.com>
2020-06-01 21:46:12 +0100

    Fix variable name typo

--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo $GCLOUD_SERVICE_KEY_TEST | base64 --decode -i > ${HOME}/gcloud-service-key.json
+echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 
 gcloud docker -- push "${REGISTRY_ADDRESS}/${REGISTRY_PROJECT_ID}/pintro-server"
