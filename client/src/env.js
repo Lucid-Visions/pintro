9b7485006a741f75afb5d25fbcfeeee2b96c007d
chris-simpkins <c.simpkins5@gmail.com>
2020-06-07 21:49:08 +0100

    Fixed expo crash issue

--- client/src/env.js
+++ client/src/env.js
@@ -1,15 +1,15 @@
 import Constants from "expo-constants";
 import Config from 'react-native-config'
+let host;
 const ssl = false
 const { manifest } = Constants;
 const production = true
-const productionHost = "3.134.100.191"
-const host = manifest.debuggerHost.split(':').shift()
+const productionHost = "34.75.14.169"
+if(!production) host = manifest.debuggerHost.split(':').shift()
 env = {
     protocol: ssl?"https":"http",
     host: production?productionHost:host,
     port:ssl?"443":"3000"
 }
-
 
 export default env
