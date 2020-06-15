63d1fbf98edf76c7548cff040b919d1676d83037
chris-simpkins <c.simpkins5@gmail.com>
2020-06-15 01:13:29 +0100

    PIN-77: Keyboard avoiding view on sign up.

--- client/src/modules/users/create/components/work-experience-screen.js
+++ client/src/modules/users/create/components/work-experience-screen.js
@@ -5,7 +5,8 @@ import {
   TouchableOpacity,
   TextInput,
   Image,
-  ScrollView
+  ScrollView,
+  KeyboardAvoidingView
 } from "react-native";
 import styles from '../../styles'
 import { yearsRegex, nameRegex } from '../../constants'
@@ -147,138 +148,140 @@ export default class workExperienceScreen extends Component {
         style={styles.container2}
         showsVerticalScrollIndicator={false}
       >
-        <View style={styles.container1}>
-          <View style={styles.container}>
-          <BackButton navigation={navigation} />
-            <View>
-              <Text style={styles.h1}>Tell us your history</Text>
-              <Text style={styles.h2}>Your work experience timeline</Text>
-            </View>
+        <KeyboardAvoidingView>
+          <View style={styles.container1}>
+            <View style={styles.container}>
+            <BackButton navigation={navigation} />
+              <View>
+                <Text style={styles.h1}>Tell us your history</Text>
+                <Text style={styles.h2}>Your work experience timeline</Text>
+              </View>
 
-            <View>
-              <Text style={styles.prompt}>Work experience</Text>
-            </View>
-            <View style={validYears==false ? styles.bottomBorderInvalid : styles.bottomBorder}>
-              <TextInput
-                style={styles.placeholder}
-                placeholderTextColor={"white"}
-                placeholder="Enter number of years"
-                keyboardType={"numeric"}
-                onChangeText={years => {
-                  validYears = fieldValidator({regex: yearsRegex, input: years})
-                  this.setState({ years })
-                }}
-                value={this.state.years}
-              />
-            </View>
-
-            <View>
-              {/* Going to be a dropdown as per PIN-79*/}
-              <Text style={styles.prompt}>Industry</Text>
-            </View>
-            <View style={validIndustry==false ? styles.bottomBorderInvalid : styles.bottomBorder}>
-              <TextInput
-                style={styles.placeholder}
-                placeholderTextColor={"white"}
-                placeholder="Enter your current industry"
-                onChangeText={industry => {
-                  validIndustry = fieldValidator({regex: nameRegex, input: industry})
-                  this.setState({ industry })
-                }}
-                value={this.state.industry}
-              />
-            </View>
-
-            <View>
-              <Text style={styles.prompt}>Previous companies</Text>
-            </View>
-            {experienceInput}
-            <TouchableOpacity
-              style={styles.imageContainer}
-              onPress={() => {
-                this.setState({
-                  experience: this.state.experience.concat({
-                    year: null,
-                    value: null
-                  })
-                });
-              }}
-            >
-              <Image
-                style={styles.image}
-                source={require("../../../../assets/whitePlus.png")}
-              />
-            </TouchableOpacity>
-
-            <View>
-              <Text style={styles.prompt}>Education</Text>
-            </View>
-            {educationInput}
-
-            <TouchableOpacity
-              style={styles.imageContainer}
-              onPress={() => {
-                this.setState({
-                  education: this.state.education.concat({
-                    year: null,
-                    value: null
-                  })
-                });
-              }}
-            >
-              <Image
-                style={styles.image}
-                source={require("../../../../assets/whitePlus.png")}
-              />
-            </TouchableOpacity>
-
-            <View>
-              <Text style={styles.prompt}>Academic Level</Text>
-            </View>
-            <View
-              style={{
-                ...styles.bottomBorder
-              }}
-            >
-              <RNPickerSelect
-                onValueChange={value => {
-                  validAcademicLevel = true
-                  this.setState({ level: value })}
-                }
-                items={[
-                  {
-                    label: "Certificate of Higher Education",
-                    value: "Certificate of Higher Education"
-                  },
-                  { label: "Foundation degree", value: "Foundation degree" },
-                  { label: "Bachelor's degree", value: "Bachelor's degree" },
-                  { label: "Master's degree", value: "Master's degree" },
-                  { label: "PhD/DPhil", value: "PhD/DPhil" }
-                ]}
-                style={pickerStyle}
-              />
-            </View>
-
-            <View paddingTop={20} alignSelf={"center"}>
-              <TouchableOpacity
-                onPress={() => this.update(navigation)}
-                underlayColor="white"
-                disabled={isSubmitDisabled()}
-              >
-                <WideButtonComponent
-                  value={"STEP 4 OF 6"}
-                  source={require("../../../../assets/arrow-right.png")}
-                  containerStyle={btnStyles}
-                  textStyle={{
-                    fontSize: 14,
-                    fontFamily: "poppins-light",
-                    color: "#1A1A1A"
+              <View>
+                <Text style={styles.prompt}>Work experience</Text>
+              </View>
+              <View style={validYears==false ? styles.bottomBorderInvalid : styles.bottomBorder}>
+                <TextInput
+                  style={styles.placeholder}
+                  placeholderTextColor={"white"}
+                  placeholder="Enter number of years"
+                  keyboardType={"numeric"}
+                  onChangeText={years => {
+                    validYears = fieldValidator({regex: yearsRegex, input: years})
+                    this.setState({ years })
                   }}
+                  value={this.state.years}
+                />
+              </View>
+
+              <View>
+                {/* Going to be a dropdown as per PIN-79*/}
+                <Text style={styles.prompt}>Industry</Text>
+              </View>
+              <View style={validIndustry==false ? styles.bottomBorderInvalid : styles.bottomBorder}>
+                <TextInput
+                  style={styles.placeholder}
+                  placeholderTextColor={"white"}
+                  placeholder="Enter your current industry"
+                  onChangeText={industry => {
+                    validIndustry = fieldValidator({regex: nameRegex, input: industry})
+                    this.setState({ industry })
+                  }}
+                  value={this.state.industry}
+                />
+              </View>
+
+              <View>
+                <Text style={styles.prompt}>Previous companies</Text>
+              </View>
+              {experienceInput}
+              <TouchableOpacity
+                style={styles.imageContainer}
+                onPress={() => {
+                  this.setState({
+                    experience: this.state.experience.concat({
+                      year: null,
+                      value: null
+                    })
+                  });
+                }}
+              >
+                <Image
+                  style={styles.image}
+                  source={require("../../../../assets/whitePlus.png")}
                 />
               </TouchableOpacity>
+
+              <View>
+                <Text style={styles.prompt}>Education</Text>
+              </View>
+              {educationInput}
+
+              <TouchableOpacity
+                style={styles.imageContainer}
+                onPress={() => {
+                  this.setState({
+                    education: this.state.education.concat({
+                      year: null,
+                      value: null
+                    })
+                  });
+                }}
+              >
+                <Image
+                  style={styles.image}
+                  source={require("../../../../assets/whitePlus.png")}
+                />
+              </TouchableOpacity>
+
+              <View>
+                <Text style={styles.prompt}>Academic Level</Text>
+              </View>
+              <View
+                style={{
+                  ...styles.bottomBorder
+                }}
+              >
+                <RNPickerSelect
+                  onValueChange={value => {
+                    validAcademicLevel = true
+                    this.setState({ level: value })}
+                  }
+                  items={[
+                    {
+                      label: "Certificate of Higher Education",
+                      value: "Certificate of Higher Education"
+                    },
+                    { label: "Foundation degree", value: "Foundation degree" },
+                    { label: "Bachelor's degree", value: "Bachelor's degree" },
+                    { label: "Master's degree", value: "Master's degree" },
+                    { label: "PhD/DPhil", value: "PhD/DPhil" }
+                  ]}
+                  style={pickerStyle}
+                />
+              </View>
+
+              <View paddingTop={20} alignSelf={"center"}>
+                <TouchableOpacity
+                  onPress={() => this.update(navigation)}
+                  underlayColor="white"
+                  disabled={isSubmitDisabled()}
+                >
+                  <WideButtonComponent
+                    value={"STEP 4 OF 6"}
+                    source={require("../../../../assets/arrow-right.png")}
+                    containerStyle={btnStyles}
+                    textStyle={{
+                      fontSize: 14,
+                      fontFamily: "poppins-light",
+                      color: "#1A1A1A"
+                    }}
+                  />
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
-        </View>
+        </KeyboardAvoidingView>
       </ScrollView>
     );
   }
