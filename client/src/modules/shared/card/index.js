87aaecc889ea5f21591c874b10253078f8bfdedf
mcpolandc <mcpolandc@gmail.com>
2020-06-18 22:04:36 +0100

    Placeholder events

--- client/src/modules/shared/card/index.js
+++ client/src/modules/shared/card/index.js
@@ -1,19 +1,12 @@
 import React from 'react'
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 
-const ImageCard = ({ title, subtitle, onPress, imgSrc, rightComponent = null }) => {
-
-    const img = imgSrc && imgSrc !== '' ? imgSrc : require("../../../assets/empty-profile-picture.png")
-    
+const Card = ({ title, subtitle, onPress, rightComponent = null }) => {
     return (
         <TouchableOpacity onPress={onPress}>
             <View style={{ borderRadius: 15, width: '100%', backgroundColor: '#FFF', marginBottom: '3%', padding: '5%'}}>
                 <View style={{ display: 'flex', flexDirection: 'row', justifyContent: "space-between" }}>
                     <View style={{ display: 'flex', flexDirection: 'row' }}>
-                        <Image
-                            style={{ height: 50, width: 50, borderRadius: 25 }}
-                            source={img}
-                        />
                         <View style={{ marginLeft: 10, marginTop: 5 }}>
                             <Text style={{ fontFamily: "poppins-semi-bold" }}>{title}</Text>
                             <Text>{subtitle}</Text>
@@ -26,4 +19,4 @@ const ImageCard = ({ title, subtitle, onPress, imgSrc, rightComponent = null }) 
     )
 }
 
-export default ImageCard
+export default Card
