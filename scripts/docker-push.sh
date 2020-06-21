504263d016eacb0fa8bf9e31af3ffa09450da948
Cary McPoland <mcpolandc@users.noreply.github.com>
2020-06-21 20:53:06 +0100

    Remove mong-seed build from CI build, will use less credits per build (#56)

--- scripts/docker-push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo $GCLOUD_SERVICE_KEY | base64 --decode -i > ${HOME}/gcloud-service-key.json
-gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
-
-gcloud docker -- push "${REGISTRY_ADDRESS}/${REGISTRY_PROJECT_ID}/pintro-server"
-gcloud docker -- push "${REGISTRY_ADDRESS}/${REGISTRY_PROJECT_ID}/mongo-seed"
