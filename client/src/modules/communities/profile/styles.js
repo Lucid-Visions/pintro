46fc23ebdf5664da8ee8b20fb72373addce58b4a
Chris Simpkins <38941066+chris-simpkins@users.noreply.github.com>
2020-06-26 00:47:17 +0100

    Profile styling updates. (#65)

--- client/src/modules/communities/profile/styles.js
+++ client/src/modules/communities/profile/styles.js
@@ -4,8 +4,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
-    marginLeft: 20,
-    marginRight: 20,
+    marginLeft: 10,
+    marginRight: 10,
     marginBottom: 30,
   },
   name: {
